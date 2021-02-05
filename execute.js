3c4a8d027cb18929c18d63b5a52fa0620930a3e9
zplusfour <zplusfour.node@outlook.com>
2021-02-05 23:03:41 +0200

    added colors

--- execute.js
+++ execute.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 
+require('colors');
+
 module.exports = (cmd) => {
   require("child_process").exec(cmd, (err, out) => {
-    if (err) throw err;
+    if (err) process.stdout.write(`Command ${err.cmd} returned an error.`.underline.red);
     process.stdout.write(out);
   });
 };
